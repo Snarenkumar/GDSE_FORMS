a030d0e9dc40baf243796f77e981a4cbc8048b0a
S Naren Kumar <142720237+Snarenkumar@users.noreply.github.com>
2024-11-21 22:05:53 +0530

    Delete GDSE_FORMS directory

--- GDSE_FORMS/forms/urls.py
+++
@@ -1,8 +0,0 @@
-from forms import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.home ,name='home'),
-    path('store',views.store ,name='store'),
-    path('export',views.export ,name='export')
-]
