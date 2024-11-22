2e38ab11bb80bd1a17e95044f8b15cb32e8e31d7
S Naren Kumar <vssnaren@gmail.com>
2024-11-22 20:31:16 +0530

    linking mysql after changes

--- GDSE_FORMS/GDSE_FORMS/settings.py
+++ GDSE_FORMS/GDSE_FORMS/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'GDSE_FORMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Gdse_forms",
+        "USER": "root",
+        "PASSWORD": "Naren@10th",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
